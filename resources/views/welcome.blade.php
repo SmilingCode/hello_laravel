c4c38ae99bcddaddcce7e54024af8774604e4121
John.xiaolu <John.xiaolu@gmail.com>
2018-03-25 02:08:57 +0000

    Reina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by John</div>
+                <div class="title">Hello My Darling! - by John</div>
             </div>
         </div>
     </body>
